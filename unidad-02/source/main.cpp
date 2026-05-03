703835441388ccb2bdbd89debcddd99341039608
julabrego <abrego.jul@gmail.com>
2026-05-02 23:42:51 -0300

    feat: implements impuse on circle

--- unidad-02/source/main.cpp
+++ unidad-02/source/main.cpp
@@ -9,54 +9,85 @@
 
 struct PhysicsBox
 {
-    b2Body* body;
-    float width;
-    float height;
-    Color color;
+	b2Body* body;
+	float width;
+	float height;
+	Color color;
 };
 
 struct PhysicsCircle
 {
-    b2Body* body;
-    float radius;
-    Color color;
+	b2Body* body;
+	float radius;
+	Color color;
 };
+
+float impulseAngle = 0.0f;
+float impulseStrength = 50000.0f;
 
 int main(void)
 {
-    const int screenWidth = 1000;
-    const int screenHeight = 600;
+	const int screenWidth = 1000;
+	const int screenHeight = 600;
 
-    InitWindow(screenWidth, screenHeight, "MAVI II - Bienvenida");
-    SetTargetFPS(60);
+	InitWindow(screenWidth, screenHeight, "MAVI II - Bienvenida");
+	SetTargetFPS(60);
 
-    Color fondo = { 110, 100, 215, 255 };
-    Color textoPrincipal = RAYWHITE;
-    Color textoSecundario = DARKPURPLE;
-    Color sueloColor = Fade(DARKGREEN, 0.7f);
+	Color fondo = { 110, 100, 215, 255 };
+	Color textoPrincipal = RAYWHITE;
+	Color textoSecundario = DARKPURPLE;
+	Color sueloColor = Fade(DARKGREEN, 0.7f);
 
-    // Mundo físico
-    b2Vec2 gravity(0.0f, 9.8f);
-    b2World world(gravity);
+	// Mundo físico
+	b2Vec2 gravity(0.0f, 9.8f);
+	b2World world(gravity);
 
-    // -----------------------------
-    // Suelo estático
-    // -----------------------------
-    b2BodyDef groundDef;
-    groundDef.type = b2_staticBody;
-    groundDef.position.Set(screenWidth / 2.0f, screenHeight - 40.0f);
-    b2Body* groundBody = world.CreateBody(&groundDef);
+	// -----------------------------
+	// Suelo estático
+	// -----------------------------
+	b2BodyDef topWallDef;
+	topWallDef.type = b2_staticBody;
+	topWallDef.position.Set(0, -20.0f);
+	b2Body* topWallBody = world.CreateBody(&topWallDef);
 
-    b2PolygonShape groundShape;
-    groundShape.SetAsBox(screenWidth / 2.0f, 20.0f);
-    groundBody->CreateFixture(&groundShape, 0.0f);
+	b2PolygonShape topWallShape;
+	topWallShape.SetAsBox(screenWidth, 20.0f);
+	topWallBody->CreateFixture(&topWallShape, 0.0f);
 
-    std::vector<PhysicsBox> boxes;
-    std::vector<PhysicsCircle> circles;
+	b2BodyDef groundDef;
+	groundDef.type = b2_staticBody;
+	groundDef.position.Set(screenWidth / 2.0f, screenHeight - 40.0f);
+	b2Body* groundBody = world.CreateBody(&groundDef);
 
-    // -----------------------------
-    // Crear un círculo
-    // -----------------------------
+	b2PolygonShape groundShape;
+	groundShape.SetAsBox(screenWidth / 2.0f, 20.0f);
+	groundBody->CreateFixture(&groundShape, 0.0f);
+
+	b2BodyDef leftWallDef;
+	leftWallDef.type = b2_staticBody;
+	leftWallDef.position.Set(-20.0f, 0);
+	b2Body* leftWallBody = world.CreateBody(&leftWallDef);
+
+	b2PolygonShape leftWallShape;
+	leftWallShape.SetAsBox(20.0f, screenHeight);
+	leftWallBody->CreateFixture(&leftWallShape, 0.0f);
+
+	b2BodyDef rightWallDef;
+	rightWallDef.type = b2_staticBody;
+	rightWallDef.position.Set(screenWidth + 20.0f, 0);
+	b2Body* rightWallBody = world.CreateBody(&rightWallDef);
+
+	b2PolygonShape rightWallShape;
+	rightWallShape.SetAsBox(20.0f, screenHeight);
+	rightWallBody->CreateFixture(&rightWallShape, 0.0f);
+
+
+	std::vector<PhysicsBox> boxes;
+	std::vector<PhysicsCircle> circles;
+
+	// -----------------------------
+	// Crear un círculo
+	// -----------------------------
 	b2BodyDef circleDef;
 	circleDef.type = b2_dynamicBody;
 	circleDef.position.Set(GetScreenWidth() / 2, 50.0f);
@@ -68,39 +99,66 @@ int main(void)
 
 	b2FixtureDef circleFixture;
 	circleFixture.shape = &circleShape;
-	circleFixture.density = 1.0f;
-	circleFixture.friction = 0.3f;
+	circleFixture.density = 0.5f;
+	circleFixture.friction = 0.5f;
 	circleFixture.restitution = 0.5f;
 
 	circleBody->CreateFixture(&circleFixture);
 
-    while (!WindowShouldClose())
-    {
-        // Avanzar simulación
-        world.Step(1.0f / 60.0f, 8, 3);
+	while (!WindowShouldClose())
+	{
+		float angleInRadians = impulseAngle * DEG2RAD;
 
-        BeginDrawing();
-        ClearBackground(fondo);
+		// Definir rotación de la próxima caja
+		if (IsKeyPressed(KEY_SPACE)) {
+			b2Vec2 impulse(impulseStrength * cosf(angleInRadians), -impulseStrength * sinf(angleInRadians));
+			circleBody->ApplyLinearImpulseToCenter(impulse, true);
+		}
+		
+		if (IsKeyDown(KEY_LEFT)) {
+			impulseAngle += 1.0f;
+		}
+		else if (IsKeyDown(KEY_RIGHT)) {
+			impulseAngle -= 1.0f;
+		}
 
-        // Suelo visual
-        DrawRectangle(0, screenHeight - 60, screenWidth, 40, sueloColor);
+		// Avanzar simulación
+		world.Step(1.0f / 60.0f, 8, 3);
 
-		DrawCircle(circleBody->GetPosition().x, circleBody->GetPosition().y, circleShape.m_radius, RED);
+		BeginDrawing();
+		ClearBackground(fondo);
+
+		// Suelo visual
+		DrawRectangle(0, screenHeight - 60, screenWidth, 40, sueloColor);
+
+		DrawCircle(circleBody->GetPosition().x, circleBody->GetPosition().y, circleShape.m_radius, BLUE);
 		DrawCircleLines(circleBody->GetPosition().x, circleBody->GetPosition().y, circleShape.m_radius, BLACK);
 
-        // Panel superior
-        DrawRectangle(90, 40, 820, 70, Fade(BLACK, 0.18f));
-        DrawCenteredText("Aplicacion de fuerzas en Box2D", 28, 60, textoPrincipal);
+		Vector2 lineStartPosition = {
+			circleBody->GetPosition().x,
+			circleBody->GetPosition().y
+		};
 
-        // Pie
-        DrawCenteredText("Flechas para cambiar direccion. Espacio para aplicar impulso", 20, 550, RAYWHITE);
-        EndDrawing();
-    }
+		Vector2 lineEndPosition = {
+			circleBody->GetPosition().x + 50.0f * cosf(angleInRadians),
+			circleBody->GetPosition().y - 50.0f * sinf(angleInRadians)
+		};
 
-    CloseWindow();
-    return 0;
+		DrawLineEx(lineStartPosition, lineEndPosition, 2.0f, GREEN);
+
+		// Panel superior
+		DrawRectangle(90, 40, 820, 70, Fade(BLACK, 0.18f));
+		DrawCenteredText("Aplicacion de fuerzas en Box2D", 28, 60, textoPrincipal);
+
+		// Pie
+		DrawCenteredText("Flechas para cambiar direccion. Espacio para aplicar impulso", 20, 550, RAYWHITE);
+		EndDrawing();
+	}
+
+	CloseWindow();
+	return 0;
 }
 
 void DrawCenteredText(const char* text, int fontSize, int posY, Color color) {
-    DrawText(text, GetScreenWidth() / 2 - MeasureText(text, fontSize) / 2, posY, fontSize, color);
+	DrawText(text, GetScreenWidth() / 2 - MeasureText(text, fontSize) / 2, posY, fontSize, color);
 }
