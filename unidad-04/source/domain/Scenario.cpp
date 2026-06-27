0d79191b6250cd232c63f08bd75d51711e3778fb
julabrego <abrego.jul@gmail.com>
2026-06-27 11:18:35 -0300

    feature: generates targets

--- unidad-04/source/domain/Scenario.cpp
+++ unidad-04/source/domain/Scenario.cpp
@@ -15,6 +15,7 @@ Scenario::Scenario(b2World& world, float screenWidth, float screenHeight)
 	CreateBoundaryWalls(world, screenWidth, screenHeight);
 	CreateStaticWalls(world);
 	CreatePlayableElements(world);
+	CreateTargets(world);
 }
 
 void Scenario::CreateWall(b2World& world, float x, float y, float halfW, float halfH)
@@ -40,6 +41,27 @@ void Scenario::CreateStaticWalls(b2World& world) {
 	ground = RectangleEntity::CreateStatic(world, 0.0f, 560.0f, GetScreenWidth(), 40.0f, 0.0f, COLOR_GROUND);
 };
 
+void Scenario::CreateTargets(b2World& world) {
+	float positions[][2] = {
+		{540.0f, 51.0f},
+		{854.0f, 110.0f},
+		{696.0f, 169.0f},
+		{540.0f, 228.0f},
+		{696.0f, 347.0f},
+		{854.0f, 288.0f},
+		{540.0f, 406.0f},
+		{854.0f, 465.0f}
+	};
+
+	for (int i = 0; i < 8; ++i) {
+		targets.push_back(std::make_unique<CircleEntity>(
+			world, positions[i][0], positions[i][1], 15.0f, RED,
+			0.0f, 0.0f, 0.0f, 0.0f, BLACK, b2_staticBody
+		));
+		BodyData* targetData = new BodyData({ BodyTag::Target });
+		targets[i]->GetBody()->GetUserData().pointer = reinterpret_cast<uintptr_t>(targetData);
+	}
+}
 
 void Scenario::CreatePlayableElements(b2World& world) {
 	spring = RectangleEntity::CreateStatic(world, 20.0f, ground.get()->position.y - 20.0f, 80.0f, 20.0f, 0.0f, COLOR_SPRING);
@@ -58,6 +80,10 @@ void Scenario::Render(Renderer& renderer)
 {
 	ground->Render(renderer);
 	spring->Render(renderer);
+
+	for (auto& target : targets) {
+		target->Render(renderer);
+	}
 
 	if (drawJoints) {
 		renderer.DrawCenteredText(("Mouse at (" + std::to_string(GetMouseX()) + ", " + std::to_string(GetMouseY()) + ")").c_str(), 20, 5, BLACK);
