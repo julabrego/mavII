7607b8d412bd49d658674379b2b4169981726d08
julabrego <abrego.jul@gmail.com>
2026-07-12 16:16:45 -0300

    refactor: extracts methods at ContactListener to reduce duplicated code

--- unidad-05/source/core/ContactListener.cpp
+++ unidad-05/source/core/ContactListener.cpp
@@ -9,108 +9,110 @@ void ContactListener::ClearFrameEvents()
 	playerStompContact = false;
 }
 
-void ContactListener::BeginContact(b2Contact* contact)
+ContactInfo ContactListener::ExtractContactInfo(b2Contact* contact)
 {
 	b2Body* bodyA = contact->GetFixtureA()->GetBody();
 	b2Body* bodyB = contact->GetFixtureB()->GetBody();
-	BodyData* dataA = reinterpret_cast<BodyData*>(bodyA->GetUserData().pointer);
-	BodyData* dataB = reinterpret_cast<BodyData*>(bodyB->GetUserData().pointer);
+	return {
+		bodyA,
+		bodyB,
+		reinterpret_cast<BodyData*>(bodyA->GetUserData().pointer),
+		reinterpret_cast<BodyData*>(bodyB->GetUserData().pointer),
+		contact->GetFixtureA()->IsSensor(),
+		contact->GetFixtureB()->IsSensor()
+	};
+}
 
-	if (!dataA || !dataB) {
-		return;
-	}
+void ContactListener::BeginContact(b2Contact* contact)
+{
+	ContactInfo info = ExtractContactInfo(contact);
+	if (!info.dataA || !info.dataB) return;
 
-	bool sensorA = contact->GetFixtureA()->IsSensor();
-	bool sensorB = contact->GetFixtureB()->IsSensor();
-	bool groundA = dataA->tag == BodyTag::Ground || dataA->tag == BodyTag::Box;
-	bool groundB = dataB->tag == BodyTag::Ground || dataB->tag == BodyTag::Box;
-
-	if ((sensorA && groundB) || (sensorB && groundA)) {
-		b2Body* sensorBody = sensorA ? bodyA : bodyB;
-		BodyData* sensorData = reinterpret_cast<BodyData*>(sensorBody->GetUserData().pointer);
-		if (sensorData && sensorData->tag == BodyTag::Player) {
-			groundContactDelta++;
-		}
-	}
-
-	bool rotablePlatformTriggererIsInArea = (dataA->tag == BodyTag::RotablePlatformTriggerArea && dataB->tag == BodyTag::RotablePlatformTriggerer)
-		|| (dataA->tag == BodyTag::RotablePlatformTriggerer && dataB->tag == BodyTag::RotablePlatformTriggerArea);
-
-	if (rotablePlatformTriggererIsInArea) {
-		isRotablePlatformTriggered = true;
-	}
-
-	bool playerVsEnemy = (dataA->tag == BodyTag::Player && dataB->tag == BodyTag::Enemy)
-		|| (dataA->tag == BodyTag::Enemy && dataB->tag == BodyTag::Player);
-
-	if (playerVsEnemy && !sensorA && !sensorB) {
-		playerVsEnemyContact = true;
-	}
-
-	bool playerEnteredFinishSensor = (dataA->tag == BodyTag::Player && dataB->tag == BodyTag::FinishSensor)
-		|| (dataA->tag == BodyTag::FinishSensor && dataB->tag == BodyTag::Player);
-
-	if (playerEnteredFinishSensor) {
-		playerReachedFinishSensor = true;
-	}
+	HandlePlayerGroundContact(contact, info, +1);
+	HandleRotablePlatformContacts(info);
+	HandlePlayerVsEnemyContact(info);
+	HandlePlayerVsFinishedSensorContact(info);
 }
 
 void ContactListener::EndContact(b2Contact* contact)
 {
-	b2Body* bodyA = contact->GetFixtureA()->GetBody();
-	b2Body* bodyB = contact->GetFixtureB()->GetBody();
-	BodyData* dataA = reinterpret_cast<BodyData*>(bodyA->GetUserData().pointer);
-	BodyData* dataB = reinterpret_cast<BodyData*>(bodyB->GetUserData().pointer);
+	ContactInfo info = ExtractContactInfo(contact);
+	if (!info.dataA || !info.dataB) return;
 
-	if (!dataA || !dataB) {
-		return;
-	}
-
-	bool sensorA = contact->GetFixtureA()->IsSensor();
-	bool sensorB = contact->GetFixtureB()->IsSensor();
-	bool groundA = dataA->tag == BodyTag::Ground || dataA->tag == BodyTag::Box;
-	bool groundB = dataB->tag == BodyTag::Ground || dataB->tag == BodyTag::Box;
-
-	if ((sensorA && groundB) || (sensorB && groundA)) {
-		b2Body* sensorBody = sensorA ? bodyA : bodyB;
-		BodyData* sensorData = reinterpret_cast<BodyData*>(sensorBody->GetUserData().pointer);
-		if (sensorData && sensorData->tag == BodyTag::Player) {
-			groundContactDelta--;
-		}
-	}
+	HandlePlayerGroundContact(contact, info, -1);
 }
 
 void ContactListener::PreSolve(b2Contact* contact, const b2Manifold* oldManifold)
 {
-	b2Body* bodyA = contact->GetFixtureA()->GetBody();
-	b2Body* bodyB = contact->GetFixtureB()->GetBody();
-	BodyData* dataA = reinterpret_cast<BodyData*>(bodyA->GetUserData().pointer);
-	BodyData* dataB = reinterpret_cast<BodyData*>(bodyB->GetUserData().pointer);
+	ContactInfo info = ExtractContactInfo(contact);
+	if (!info.dataA || !info.dataB) return;
 
-	bool isPlayerBodyA = dataA && dataA->tag == BodyTag::Player;
-	bool isEnemyBodyA = dataA && dataA->tag == BodyTag::Enemy;
-	bool isPlayerBodyB = dataB && dataB->tag == BodyTag::Player;
-	bool isEnemyBodyB = dataB && dataB->tag == BodyTag::Enemy;
+	HandleEnemyStompContact(info);
+	HandlePlayerAgainstWallFrictionContact(contact, info);
+}
 
-	if ((isPlayerBodyA && isEnemyBodyB) || (isEnemyBodyA && isPlayerBodyB)) {
-		bool fixtureSensorA = contact->GetFixtureA()->IsSensor();
-		bool fixtureSensorB = contact->GetFixtureB()->IsSensor();
-		if (!fixtureSensorA && !fixtureSensorB) {
-			b2Body* playerBody = isPlayerBodyA ? bodyA : bodyB;
+void ContactListener::HandlePlayerGroundContact(b2Contact* contact, const ContactInfo& info, int sign) {
+	bool groundA = info.dataA->tag == BodyTag::Ground || info.dataA->tag == BodyTag::Box;
+	bool groundB = info.dataB->tag == BodyTag::Ground || info.dataB->tag == BodyTag::Box;
+
+	if ((info.sensorA && groundB) || (info.sensorB && groundA)) {
+		b2Body* sensorBody = info.sensorA ? info.bodyA : info.bodyB;
+		BodyData* sensorData = reinterpret_cast<BodyData*>(sensorBody->GetUserData().pointer);
+		if (sensorData && sensorData->tag == BodyTag::Player) {
+			groundContactDelta += sign;
+		}
+	}
+}
+
+void ContactListener::HandleRotablePlatformContacts(const ContactInfo& info) {
+	bool trigger = (info.dataA->tag == BodyTag::RotablePlatformTriggerArea && info.dataB->tag == BodyTag::RotablePlatformTriggerer)
+		|| (info.dataA->tag == BodyTag::RotablePlatformTriggerer && info.dataB->tag == BodyTag::RotablePlatformTriggerArea);
+
+	if (trigger) {
+		isRotablePlatformTriggered = true;
+	}
+}
+
+void ContactListener::HandlePlayerVsEnemyContact(const ContactInfo& info) {
+	bool playerVsEnemy = (info.dataA->tag == BodyTag::Player && info.dataB->tag == BodyTag::Enemy)
+		|| (info.dataA->tag == BodyTag::Enemy && info.dataB->tag == BodyTag::Player);
+
+	if (playerVsEnemy && !info.sensorA && !info.sensorB) {
+		playerVsEnemyContact = true;
+	}
+}
+
+void ContactListener::HandleEnemyStompContact(const ContactInfo& info) {
+	bool isPlayerA = info.dataA->tag == BodyTag::Player;
+	bool isEnemyA = info.dataA->tag == BodyTag::Enemy;
+	bool isPlayerB = info.dataB->tag == BodyTag::Player;
+	bool isEnemyB = info.dataB->tag == BodyTag::Enemy;
+
+	if ((isPlayerA && isEnemyB) || (isEnemyA && isPlayerB)) {
+		if (!info.sensorA && !info.sensorB) {
+			b2Body* playerBody = isPlayerA ? info.bodyA : info.bodyB;
 			if (playerBody->GetLinearVelocity().y > 1.0f) {
 				playerStompContact = true;
 			}
 		}
 	}
+}
 
-	bool playerA = dataA && dataA->tag == BodyTag::Player;
-	bool playerB = dataB && dataB->tag == BodyTag::Player;
+void ContactListener::HandlePlayerAgainstWallFrictionContact(b2Contact* contact, const ContactInfo& info) {
+	bool isPlayerA = info.dataA->tag == BodyTag::Player;
+	bool isPlayerB = info.dataB->tag == BodyTag::Player;
 
-	if (playerA || playerB)
-	{
-		if (contact->GetFixtureA()->IsSensor() || contact->GetFixtureB()->IsSensor())
-			return;
-
+	if (isPlayerA || isPlayerB) {
+		if (info.sensorA || info.sensorB) return;
 		contact->SetFriction(0.0f);
+	}
+}
+
+void ContactListener::HandlePlayerVsFinishedSensorContact(const ContactInfo& info) {
+	bool finish = (info.dataA->tag == BodyTag::Player && info.dataB->tag == BodyTag::FinishSensor)
+		|| (info.dataA->tag == BodyTag::FinishSensor && info.dataB->tag == BodyTag::Player);
+
+	if (finish) {
+		playerReachedFinishSensor = true;
 	}
 }
--- unidad-05/source/core/ContactListener.h
+++ unidad-05/source/core/ContactListener.h
@@ -1,5 +1,15 @@
 #pragma once
 #include <box2d.h>
+#include "BodyData.h"
+
+struct ContactInfo {
+    b2Body* bodyA;
+    b2Body* bodyB;
+    BodyData* dataA;
+    BodyData* dataB;
+    bool sensorA;
+    bool sensorB;
+};
 
 class ContactListener : public b2ContactListener
 {
@@ -7,14 +17,22 @@ public:
 	void BeginContact(b2Contact* contact) override;
 	void EndContact(b2Contact* contact) override;
 	void PreSolve(b2Contact* contact, const b2Manifold* oldManifold) override;
-
 	void ClearFrameEvents();
 
+	int groundContactDelta = 0;
+	
 	bool isRotablePlatformTriggered = false;
 	bool playerReachedFinishSensor = false;
-
-	int groundContactDelta = 0;
 	bool playerVsEnemyContact = false;
 	bool playerStompContact = false;
-};
 
+private:
+	ContactInfo ExtractContactInfo(b2Contact* contact);
+
+	void HandlePlayerGroundContact(b2Contact* contact, const ContactInfo& info, int sign);
+	void HandleRotablePlatformContacts(const ContactInfo& info);
+	void HandlePlayerVsEnemyContact(const ContactInfo& info);
+	void HandleEnemyStompContact(const ContactInfo& info);
+	void HandlePlayerAgainstWallFrictionContact(b2Contact* contact, const ContactInfo& info);
+	void HandlePlayerVsFinishedSensorContact(const ContactInfo& info);
+};
