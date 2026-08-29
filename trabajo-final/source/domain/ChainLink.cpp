1fadb014c84f53c81363875dd7ec77877c1d0f69
julabrego <abrego.jul@gmail.com>
2026-08-28 23:08:56 -0300

    fix: removes unnecessary radius property from ChainLink

--- trabajo-final/source/domain/ChainLink.cpp
+++ trabajo-final/source/domain/ChainLink.cpp
@@ -2,8 +2,8 @@
 #include "../core/PhysicsConstants.h"
 #include <cmath>
 
-ChainLink::ChainLink(b2World& world, float startX, float startY, float angle, float radius, float density, float friction, float restitution, float linearDamping, float angularDamping)
-	: radius(radius), density(density), friction(friction), restitution(restitution), linearDamping(linearDamping), angularDamping(angularDamping)
+ChainLink::ChainLink(b2World& world, float startX, float startY, float angle, float density, float friction, float restitution, float linearDamping, float angularDamping)
+	: density(density), friction(friction), restitution(restitution), linearDamping(linearDamping), angularDamping(angularDamping)
 {
 	hitbox = RectangleEntity::CreateDynamic(world, startX - LINK_WIDTH / 2.0f, startY - LINK_HEIGHT / 2.0f, LINK_WIDTH, LINK_HEIGHT, angle * RAD2DEG, Fade(BLACK, 0.2f), density, friction, restitution);
 	hitbox->GetBody()->SetLinearDamping(linearDamping);
--- trabajo-final/source/domain/ChainLink.h
+++ trabajo-final/source/domain/ChainLink.h
@@ -6,7 +6,6 @@
 #include "../core/BodyData.h"
 #include "../game/GameState.h"
 
-const float LINK_DEFAULT_RADIUS = 7.0f;
 const float LINK_DEFAULT_DENSITY = 2.0f;
 const float LINK_DEFAULT_FRICTION = 1.0f;
 const float LINK_DEFAULT_RESTITUTION = 0.5f;
@@ -19,7 +18,7 @@ class ChainLink :
 	public Entity
 {
 public:
-	ChainLink(b2World& world, float startX, float startY, float angle = 0.0f, float radius = LINK_DEFAULT_RADIUS, float density = LINK_DEFAULT_DENSITY, float friction = LINK_DEFAULT_FRICTION, float restitution = LINK_DEFAULT_RESTITUTION, float linearDamping = LINK_DEFAULT_LINEAR_DAMPING, float angularDamping = LINK_DEFAULT_ANGULAR_DAMPING);
+	ChainLink(b2World& world, float startX, float startY, float angle = 0.0f, float density = LINK_DEFAULT_DENSITY, float friction = LINK_DEFAULT_FRICTION, float restitution = LINK_DEFAULT_RESTITUTION, float linearDamping = LINK_DEFAULT_LINEAR_DAMPING, float angularDamping = LINK_DEFAULT_ANGULAR_DAMPING);
 	~ChainLink();
 	void Update(float deltaTime) override;
 	void Render(Renderer& renderer) override;
@@ -30,7 +29,6 @@ private:
 	std::unique_ptr<RectangleEntity> hitbox;
 	BodyData bodyData;
 
-	float radius = LINK_DEFAULT_RADIUS;
 	float density = LINK_DEFAULT_DENSITY;
 	float friction = LINK_DEFAULT_FRICTION;
 	float restitution = LINK_DEFAULT_RESTITUTION;
